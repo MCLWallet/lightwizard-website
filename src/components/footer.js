54805fcb857e4f2883979113d8200974c87e6a8c
Marcell Lanczos <MCL@Marcells-MacBook-Pro.local>
2020-07-10 23:23:38 +0200

    Todos + styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,14 +7,16 @@ const Footer = () => (
   <footer>
       <ul>
         <li>
-          <a href="#">facebook</a>
+          <a href="https://www.facebook.com/lightwizardvienna" target="_blank">facebook</a>
         </li>
         <li>
-          <a href="#">instagram</a>
+          <a href="https://www.instagram.com/light_wizard_vienna/" target="_blank">instagram</a>
         </li>
+        {/* TODO: Pinterest public board? */}
         <li>
           <a href="#">pinterest</a>
         </li>
+        {/* TODO: get new domain */}
         <li>
           <a href="#">mail</a>
         </li>
@@ -24,13 +26,13 @@ const Footer = () => (
           <Link to="/" activeClassName="active">home</Link>
         </li>
         <li>
-          <Link to="/page-2" activeClassName="active" > portfolio </Link>
+          <Link to="/portfolio" activeClassName="active" > portfolio </Link>
         </li>
         <li>
-          <Link to = "/page-2" activeClassName="active"> about </Link>
+          <Link to = "/about" activeClassName="active"> about </Link>
         </li>
         <li>
-          <Link to = "/page-2" activeClassName="active"> contact </Link>
+          <Link to = "/contact" activeClassName="active"> contact </Link>
         </li>
       </ul>
   </footer>
